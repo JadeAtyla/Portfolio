8324f9a2dc5d9839c238db085399662b41bd47b9
JadeAtyla <jamadigal@gmail.com>
2024-10-30 21:54:38 +0800

    Updated Portfolio

---
+++ errorhandler.js
@@ -0,0 +1,4 @@
+window.onerror = function (){
+    window.location.href = "errorhandler.html";
+    return true;
+}
--- script.js
+++ script.js
@@ -7,7 +7,22 @@ function toggleMenu() {
 
 function downloadCV() {
   const cv = document.getElementById("cv");
-  cv.href = "assets/Jade Atyla Madigal - Resume.pdf";
-  cv.download = "Jade Atyla Madigal - Resume.pdf";
-  cv.click();
+  const link = document.createElement("a");
+  link.href = "assets/Jade Atyla Madigal - Resume.pdf";
+  link.download = "Jade Atyla Madigal - Resume.pdf";
+  
+  cv.appendChild(link);
+  link.click();
+
+  cv.removeChild(link);
+}
+
+function toProject(type){
+  const project = `projects.html?title=${type}`
+  window.location.href = `loading.html?link=${project}`;
+}
+
+function returnMainPage(){
+  const link = 'index.html';
+  window.location.href = `loading.html?link=${link}`;
 }
